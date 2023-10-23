3855167008006b1a8129f06ba0d42d8d1d13245a
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2023-10-23 11:36:16 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximum sum increasing subsequence/maximum-sum-increasing-subsequence.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+		
+
+	public:
+	int maxSumIS(int arr[], int n)  
+	{  
+	    // Your code goes here
+	       int ans=arr[0];
+   //copy the arr to vector dp
+    vector<int> v(arr,arr+n);
+ 
+    for(int i=1;i<n;i++){
+        for(int j=i-1;j>=0;j--){
+ 
+            //use the previous result to update the current position
+            if(arr[j]<arr[i]){
+                v[i]=max(v[i],v[j]+arr[i]);
+            }
+        }
+        ans=max(ans,v[i]);
+    }
+    return ans;
+	}  
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int a[n];
+
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+      
+
+	    Solution ob;
+	    cout << ob.maxSumIS(a, n) << "\n";
+	     
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
