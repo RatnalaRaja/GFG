581e302a18f57c125ef7bade2ed98bf27d08724d
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2024-01-14 20:47:34 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Remove Duplicates/remove-duplicates.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+		
+	string removeDups(string S) 
+	{
+	    // Your code goes here
+	     string ans ;
+        unordered_map<char,int> mp ;
+        for(auto i: S) {
+            if(++mp[i] == 1) ans += i ;
+        }
+        return ans ;
+	}
+};
+
+//{ Driver Code Starts.
+
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   		
+   		Solution ob;
+   		cout << ob.removeDups(s) << "\n";
+   		
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
